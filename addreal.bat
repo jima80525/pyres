5246cb54fd7f71681164d655e01ffa442a2842cd
Jim Anderson <jima80525+pyres@gmail.com>
2014-10-06 20:04:17 -0600

    added the script to add all of my podcasts

---
+++ addreal.bat
@@ -0,0 +1,17 @@
+@echo OFF
+REM
+\Python27\python main.py -a http://rss.sciam.com/sciam/60-second-psych --start-date 09/07/14
+\Python27\python main.py -a http://rss.sciam.com/sciam/60secsciencepodcast --start-date 09/27/14
+\Python27\python main.py -a http://feeds.99percentinvisible.org/99percentinvisible --start-date 08/01/14
+\Python27\python main.py -a http://feeds2.feedburner.com/BackStoryRadio --start-date 09/27/14
+\Python27\python main.py -a http://www.npr.org/rss/podcast.php?id=13 --start-date 09/28/14
+\Python27\python main.py -a http://feeds.hearstartup.com/hearstartup --start-date 09/28/14
+\Python27\python main.py -a http://thehistoryofbyzantium.libsyn.com/rss --start-date 07/01/14
+\Python27\python main.py -a http://www.npr.org/rss/podcast.php?id=510303 --start-date 09/28/14
+\Python27\python main.py -a http://www.npr.org/rss/podcast.php?id=5194672 --start-date 09/28/13
+\Python27\python main.py -a http://www.npr.org/rss/podcast.php?id=510298 --start-date 09/28/14
+\Python27\python main.py -a http://feeds.wnyc.org/radiolab --start-date 09/28/14
+\Python27\python main.py -a http://revolutionspodcast.libsyn.com/rss --start-date 09/23/14
+\Python27\python main.py -a http://feeds.feedburner.com/TheMemoryPalace --start-date 07/28/14
+\Python27\python main.py -a http://feeds.thisamericanlife.org/talpodcast --start-date 09/27/14
+\Python27\python main.py -a http://www.npr.org/rss/podcast.php?id=35 --start-date 09/28/14
