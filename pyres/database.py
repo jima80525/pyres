18ec0275307ef847d788706b83976c379f9d4922
Jim Anderson <jima.coding@gmail.com>
2019-07-20 15:46:07 -0600

    Testing pre-commit hook again

--- pyres/database.py
+++ pyres/database.py
@@ -157,7 +157,6 @@ class PodcastDatabase(object):
         episode.state = 2
         episode.save()
 
-    # JHA TODO - perhaps we can add these directly to Episode??
     def update_size(self, episode, size):
         episode.size = size
         episode.save()
